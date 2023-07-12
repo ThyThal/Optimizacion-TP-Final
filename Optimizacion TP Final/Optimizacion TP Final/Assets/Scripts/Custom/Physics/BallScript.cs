e3cd472ead835fedef4cd0056e150799a9b9d2a5
agustin <agustincavalie14@gmail.com>
2023-07-12 02:25:36 -0300

    1

---
+++ Optimizacion TP Final/Optimizacion TP Final/Assets/Scripts/Custom/Physics/BallScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviourGameplay
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void ManagedUpdate()
+    {
+        
+
+
+    }
+         
+    
+}
---
+++ Optimizacion TP Final/Optimizacion TP Final/Assets/Scripts/Custom/Physics/Collider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    
+}
