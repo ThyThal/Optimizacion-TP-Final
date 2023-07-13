bbb2ec9fc3d9728ca91a168af9463168abe4079d
agustin <agustincavalie14@gmail.com>
2023-07-12 23:06:02 -0300

    Rebote de la pelota y brick basico creado

--- Optimizacion TP Final/Optimizacion TP Final/Assets/Scripts/Custom/Physics/BallScript.cs
+++ Optimizacion TP Final/Optimizacion TP Final/Assets/Scripts/Custom/Physics/BallScript.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviourGameplay
 {
-    
+    public float speed = 5f;
+
+    private Vector2 direction;
+
+    public float wallPosition;
+    public float ceilingPosition;
 
     void Start()
     {
-        
+        direction = new Vector2(1f, 1f).normalized;
     }
 
     // Update is called once per frame
     public override void ManagedUpdate()
     {
-        
 
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.x >= wallPosition || transform.position.x <= -wallPosition)
+        {           
+            direction.x = -direction.x;
+        }
+
+        if (transform.position.y >= ceilingPosition || transform.position.y <= -ceilingPosition)
+        {
+            direction.y = -direction.y;
+        }
 
     }
-         
+
+    public void ChangeDirecction(Vector2 impactedWall) 
+    {
+        if (impactedWall == Vector2.up || impactedWall == Vector2.down) 
+        {
+            direction.y = -direction.y;
+        }
+
+        if (impactedWall == Vector2.left || impactedWall == Vector2.right) 
+        {
+            direction.x = -direction.x;
+        }
+    }
     
 }
---
+++ Optimizacion TP Final/Optimizacion TP Final/Assets/Scripts/Custom/Physics/BrickColliderScript.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickColliderScript : MonoBehaviourGameplay
+{
+    public GameObject ball;
+    private BallScript ballScript;
+    public Transform brick;
+    public float ballRadius = 0.5f;
+    public float tolerance = 0.5f;
+    public float maxDistance = 1f;
+    public Vector2 impactNormal;
+
+    private void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+        ballScript = ball.GetComponent<BallScript>();
+    }
+    public override void ManagedUpdate()
+    {
+        if (IsBallTouchingBrick(ball.transform.position, brick.position, brick.localScale, maxDistance))
+        {
+            ballScript.ChangeDirecction(impactNormal);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private bool IsBallTouchingBrick(Vector3 ballPosition, Vector3 brickPosition, Vector3 brickScale, float maxDistance)
+    {
+        float halfWidth = brickScale.x / 2f;
+        float halfHeight = brickScale.y / 2f;
+
+        float leftEdge = brickPosition.x - halfWidth - ballRadius;
+        float rightEdge = brickPosition.x + halfWidth + ballRadius;
+        float topEdge = brickPosition.y + halfHeight + ballRadius;
+        float bottomEdge = brickPosition.y - halfHeight - ballRadius;
+
+        bool isWithinHorizontalRange = ballPosition.x >= (leftEdge - tolerance) && ballPosition.x <= (rightEdge + tolerance);
+        bool isWithinVerticalRange = ballPosition.y >= (bottomEdge - tolerance) && ballPosition.y <= (topEdge + tolerance);
+        bool isWithinMaxDistance = Vector3.Distance(ballPosition, brickPosition) <= maxDistance;
+
+        impactNormal = Vector2.zero;
+
+        if (isWithinHorizontalRange && isWithinVerticalRange && isWithinMaxDistance)
+        {
+            if (Mathf.Abs(ballPosition.x - leftEdge) <= tolerance)
+            {
+                impactNormal = Vector2.left;
+            }
+            else if (Mathf.Abs(ballPosition.x - rightEdge) <= tolerance)
+            {
+                impactNormal = Vector2.right;
+            }
+            else if (Mathf.Abs(ballPosition.y - topEdge) <= tolerance)
+            {
+                impactNormal = Vector2.up;
+            }
+            else if (Mathf.Abs(ballPosition.y - bottomEdge) <= tolerance)
+            {
+                impactNormal = Vector2.down;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
