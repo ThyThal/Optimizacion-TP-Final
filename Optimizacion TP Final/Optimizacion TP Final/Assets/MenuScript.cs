6f92a38f81e7e5d1ae2c6aed2993519aa6a820d3
agustin <agustincavalie14@gmail.com>
2023-07-14 02:35:05 -0300

    Menu creado
    Menu sencillo que solo se controla con el menu. Son dos imagenes para ser lo mas simple posible

---
+++ Optimizacion TP Final/Optimizacion TP Final/Assets/MenuScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class MenuScript : MonoBehaviourUI
+{
+    public GameObject Credits;
+
+    public override void ManagedUpdate() 
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            Credits.SetActive(true);
+        }
+        else 
+        {
+            Credits.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            ExitGame();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            LoadSceneLevel();
+        }
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+
+    public void LoadSceneLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
